db1dd74f1344c21f569563a691725cab7ac2d913
kashishRamnani <kashishramnani337@gmail.com>
2026-02-06 17:50:18 +0500

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {  Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import { Home, AboutMe,Projects,Skills,Contact} from "./pages";
