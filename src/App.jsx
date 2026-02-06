2c2ccf945b68fc2abf1f0b85b8caac676e75c62e
kashishRamnani <kashishramnani337@gmail.com>
2026-02-06 17:58:09 +0500

    Remove nested Router to fix GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import {  Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; // no Router import
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import { Home, AboutMe,Projects,Skills,Contact} from "./pages";
+import { Home, AboutMe, Projects, Skills, Contact } from "./pages";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutMe />} />
-         <Route path="/projects" element={<Projects />} />
-         <Route path="/skills" element={<Skills />} />
-         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       <Footer />
-    </Router>
+    </>
   );
 }
 
